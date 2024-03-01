51301499ebcc3678a44b154b82cf8649e265d426
devyogi402 <yogidev402@gmail.com>
2024-03-01 23:31:52 +0530

    Update mail.php
    Changes in male page

--- assets/mail.php
+++ assets/mail.php
@@ -2,8 +2,7 @@
 $to = "pkumarskp4@gmail.com";
 $subject = $_POST['subject'];
 $txt = $_POST['message'];
-$headers = "From: $_POST['email'];. "\r\n" .
-"CC: somebodyelse@example.com";
+$headers = "From:". $_POST['email'];
 mail($to,$subject,$txt,$headers);
 header('Location: index.html');
 ?>
